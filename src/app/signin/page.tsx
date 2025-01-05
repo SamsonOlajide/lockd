1a1301bf6c473473e49b0c412d55c0abdf9d1ba3
Samson <samsonolajide03@gmail.com>
2025-01-05 12:58:14 +0000

    sign in page for registerd users and minor tweaks on sign up page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -14,8 +14,8 @@ export default function Signup() {
             <div className="flex justify-evenly items-center h-[85vh]">
                 <div className="max-w-[400px]">
                     <div className="mb-5">
-                        <h1 className="font-pixel text-5xl text-center ">Get Started</h1>
-                        <p className="font-inter text-center">Create a new account</p>
+                        <h1 className="font-pixel text-5xl text-center ">Welcome Back</h1>
+                        <p className="font-inter text-center">Sign in to your account</p>
                     </div>
                     <div className="flex  items-center justify-center rounded-md py-[6.5px] border-black border-[0.5px] mb-2 shadow-md">
                         <Image src='/googleLogo.svg' alt="rdmn" width={31} height={10}/>
@@ -30,23 +30,22 @@ export default function Signup() {
 
                     <form>
                         <div className="mb-2">
-                            <label className="font-inter block" htmlFor="name">Name</label>
-                            <input className="font-inter border-black border-[0.5px] mb-2 shadow-md h-[41px] w-[400px] pl-2 bg-oat rounded-md" type="text" id="name" name="name" />
-                        </div>
-
-                        <div className="mb-2">
                             <label className="font-inter block"  htmlFor="email">Email</label>
                             <input type="email" id="email" name="email" className="border-black border-[0.5px] mb-2 shadow-md h-[41px] w-[400px] pl-2 bg-oat rounded-md"/>
                         </div>
 
                         <div className="mb-2">
-                            <label className="font-inter block" htmlFor="password">Password</label>
+                            <div className="flex justify-between">
+                                <label className="font-inter" htmlFor="password">Password</label>
+                                <p className="justify-center text-[13px] items-center content-center font-inter">Forgot Password?</p>
+                            </div>                    
                             <input type="password" id="password" name="password" className="border-black border-[0.5px] shadow-md h-[41px] w-[400px] mb-[50px] pl-2 bg-oat rounded-md"/>
                         </div>
-                        <button type="submit" className="bg-black w-[400px] text-oat font-pixel text-[28px] rounded-md shadow-md h-[48px]">Sign Up</button>
+                        <button type="submit" className="bg-black w-[400px] text-oat font-pixel text-[28px] rounded-md shadow-md h-[48px]">Sign In</button>
                     </form>
-                    <Link href="/signin">
-                        <p className="text-center w-[400px] mt-4">Have an account? <span className="underline">Sign in Now</span></p>
+
+                    <Link href="/signup">
+                        <p className="text-center w-[400px] mt-4">Don’t have an account? <span className="underline">Sign up Now</span></p>
                     </Link>
                 </div>
             </div>
